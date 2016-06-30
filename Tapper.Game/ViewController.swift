43a7baba098afd87dcb8bcfa93bfdf34de9c570c
ScottieA11 <Scottiea815@yahoo.com>
2016-06-29 23:17:04 -0500

    Game updated with code.

--- Tapper.Game/ViewController.swift
+++ Tapper.Game/ViewController.swift
@@ -10,10 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+//Variables 
+    
+    var maxAdd = 0
+    var currentAddt = 0
+    
+// Outlets
+    @IBOutlet weak var Logo: UIImageView!
+    @IBOutlet weak var inputTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var multiTxt: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
     
     
+    
+    @IBAction func onAddPressed(sender: UIButton!){
+        currentAddt += 1
+        
+    }
+    
+    
+    @IBAction func addBtnPressed(sender: AnyObject) {
 
+       if inputTxt.text != nil && inputTxt.text != ""{
+        
+            Logo.hidden = true
+            inputTxt.hidden = true
+            playBtn.hidden = true
+        
+            multiTxt.hidden = false
+            addBtn.hidden = false
+        
+            maxAdd = Int(inputTxt.text!)!
+        
+        
+        
+        
+        }
+        
+    }
+    
+    
+    func restartAddingGame(){
+        
+        
+    }
 
+    func checkGameStatus(){
+        
+    }
+    
 }
 
